feff298e37c5fab80edca2c77a84aa4c4fdf4c57
DESKTOP-8F9VMMN\hnJin <knsan189@gmail.com>
2022-07-05 11:40:57 +0900

    edit package.json

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { Box, LinearProgress, Typography } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Result from "../components/Result/Result";
 import Search from "../components/Search/Search";
